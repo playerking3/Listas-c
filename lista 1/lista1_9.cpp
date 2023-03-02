d2d5207ce3d4787e5a1faeb616bba2465a65582f
playerking3 <murilomarquesdecastro@gmail.com>
2023-03-02 09:26:19 -0300

    armstrong feito sem gambiarra

--- lista 1/lista1_9.cpp
+++ lista 1/lista1_9.cpp
@@ -4,28 +4,17 @@ using namespace std;
 
 main(){
 	setlocale(LC_ALL, "portuguese");
-	int digitos, user, armstrong, ndigi;
+	int digitos, user, armstrong, ndigi = 0, expo = 1;
 	int casa = 1;	
 	
 	cout<<"Insira um numero: ";
 	cin>>user;
 	
-	//gambiarra
-	if(user>=0 && user < 10){
-		ndigi = 1;
-	}else if(user>=10 && user < 100){
-		ndigi = 2;
-	}else if(user>=100 && user < 1000){
-		ndigi = 3;
-	}else if(user>=1000 && user < 10000){
-		ndigi = 4;
-	}else if(user>=10000 && user < 100000){
-		ndigi = 5;
-	}else if(user>=100000 && user < 1000000){
-		ndigi = 6;
+	while(user> expo){
+		expo *= 10;
+		ndigi ++;
 	}
-	//gmabiarra
-	
+		
 	while(armstrong < user){
 		
 		digitos = (user/casa)%10;
@@ -37,10 +26,6 @@ main(){
 			break;
 		}
 		casa *= 10;
-		
-		cout<<armstrong<<endl;
-		cout<<digitos<<endl;
-		cout<<casa<<endl;
 	}
 	
 	if(armstrong== user){
